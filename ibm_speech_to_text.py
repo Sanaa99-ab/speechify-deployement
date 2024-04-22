7748e39608d26a0a2c947651b68df7e43997a071
Sanaa ABRIL <63234660+Sanaa99-ab@users.noreply.github.com>
2024-04-22 20:54:11 +0100

    Update ibm_speech_to_text.py

--- ibm_speech_to_text.py
+++ ibm_speech_to_text.py
@@ -166,7 +166,7 @@ models = {
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('/templates/index.html')
 
 @app.route('/convert_audio', methods=['POST'])
 def convert_audio():
